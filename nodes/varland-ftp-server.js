fb8415342c8771c6c5150c09e340d04ebf7377dc
Toby Varland <toby.varland@varland.com>
2021-10-12 15:56:24 -0400

    Modify varland FTP server configuration node to use credentials object

--- nodes/varland-ftp-server.js
+++ nodes/varland-ftp-server.js
@@ -3,8 +3,8 @@ module.exports = function(RED) {
   function VarlandFTPServerNode(n) {
       RED.nodes.createNode(this, n);
       this.hostname = n.hostname;
-      this.username = n.username;
-      this.password = n.password;
+      this.username = n.credentials.username;
+      this.password = n.credentials.password;
       this.rootPath = n.rootPath;
   }
   RED.nodes.registerType("varland ftp server",
