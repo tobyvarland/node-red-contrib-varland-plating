0fc908fdf98bd1832160d4468d44225370a3a69f
Toby Varland <toby.varland@varland.com>
2021-10-12 20:56:59 -0400

    Add recipe file duplication to FTP server

--- nodes/preserve-recipe-data.js
+++ nodes/preserve-recipe-data.js
@@ -7,6 +7,7 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
 
     this.groovController = RED.nodes.getNode(config.groovController);
+    this.ftpServer = RED.nodes.getNode(config.ftpServer);
 
     var node = this;
 
@@ -17,6 +18,10 @@ module.exports = function(RED) {
       const preserver = new GroovPreserver({
         apiKey: node.groovController.apiKey,
         hostname: node.groovController.hostname
+      }, {
+        hostname: node.ftpServer.hostname,
+        username: node.ftpServer.username,
+        password: node.ftpServer.password
       });
       preserver.process();
 
--- nodes/varland-ftp-server.js
+++ nodes/varland-ftp-server.js
@@ -3,9 +3,8 @@ module.exports = function(RED) {
   function VarlandFTPServerNode(n) {
       RED.nodes.createNode(this, n);
       this.hostname = n.hostname;
-      this.username = n.credentials.username;
-      this.password = n.credentials.password;
-      this.rootPath = n.rootPath;
+      this.username = n.username;
+      this.password = n.password;
   }
   RED.nodes.registerType("varland ftp server",
                          VarlandFTPServerNode);
